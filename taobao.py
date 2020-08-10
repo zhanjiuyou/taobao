78f34592f391ef7d777330d886aa815177dede44
zhanjiuyou <794651614@qq.com>
2020-08-10 22:17:00 +0800

    Initial commit

---
+++ taobao.py
@@ -0,0 +1,11 @@
+#2020年8月10日
+#该爬虫主要完成淘宝网站的登录商品销量和排行等爬取
+#由于淘宝网站是异步加载的，只能使用selenium调用浏览器爬取
+#我的个人博客: https://jiaokangyang.com
+
+
+
+import requests
+from selenium import webdriver
+
+s = webdriver.Chrome()
